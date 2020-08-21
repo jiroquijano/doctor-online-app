aaf942f7e1baacb1bf6b8f9f791b1bf8da3eb821
jiroquijano <jiroquijano@gmail.com>
2020-08-21 14:28:33 +0800

    wired up RegistrationForm component to backend

--- client/src/components/RegistrationForm.js
+++ client/src/components/RegistrationForm.js
@@ -1,29 +1,64 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
+import useAxios from '../customhooks/useAxios';
+import {useHistory} from 'react-router-dom';
 
 const RegistrationForm = () => {
-    const [userType, setUserType] = useState('');
+    const [accountType, setAccountType] = useState('patient');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [specializations, setSpecializations] = useState([]);
     const [licenceNumber, setLicenceNumber] = useState('');
     const [name, setName] = useState({});
+    const [url, setUrl] = useState('');
+    const [options, setOptions] = useState({});
+    const {response, error, loading} = useAxios(url, options);
+    let history = useHistory();
+
+    useEffect(()=>{
+        if(response){
+            console.log(response);
+            history.push('/');
+        }
+        if(error){
+            console.log(error);
+        }
+    },[response,error,history]);
+
+    useEffect(()=>{
+        if(loading === false){
+            setAccountType('patient');
+            setEmail('');
+            setPassword('');
+            setSpecializations([]);
+            setLicenceNumber('');
+            setName({});
+        }
+    },[loading]);
 
     const onSubmitHandler = (e)=>{
         e.preventDefault();
         let registrationDetails = {
-            userType,
+            accountType,
             email,
             password,
             name
         };
-        if(userType === 'doctor') {
+        if(accountType === 'doctor') {
             registrationDetails = {
                 ...registrationDetails,
                 specializations,
                 licenceNumber
             };
         }
-        console.log(registrationDetails);
+        const data = JSON.stringify(registrationDetails);
+        setUrl('/api/register');
+        setOptions({
+            method: 'post',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            data : data
+        });
     };
 
     return (
@@ -31,15 +66,18 @@ const RegistrationForm = () => {
             <div>
                 <label>
                     I would like to register as a:
-                    <select name="usertype" onChange={(e)=>{
-                        setUserType(e.target.value)
+                    <select 
+                        name="accountType"
+                        value={accountType}
+                        onChange={(e)=>{
+                        setAccountType(e.target.value)
                     }}>
                         <option value="patient">Patient</option>
                         <option value="doctor">Doctor</option>
                     </select>
                 </label>
                 <div>
-                    {userType ==='doctor' ? (
+                    {accountType ==='doctor' ? (
                         <>
                             <label>
                                 Specialization/s:
@@ -47,6 +85,7 @@ const RegistrationForm = () => {
                                     type="text"
                                     placeholder="(separate with commas)"
                                     name="specializations"
+                                    value={specializations}
                                     onChange={(e)=>{
                                         setSpecializations(e.target.value.split(',').map(curr=>curr.trim()))
                                     }}
@@ -57,6 +96,7 @@ const RegistrationForm = () => {
                                 <input 
                                     type="text"
                                     placeholder="valid licence number"
+                                    value={licenceNumber}
                                     name="licence"
                                     required={true}
                                     minLength={3}
@@ -75,6 +115,7 @@ const RegistrationForm = () => {
                     <input type="email"
                         placeholder="email address"
                         name="email"
+                        value={email}
                         onChange={(e)=>{
                             setEmail(e.target.value)
                         }}
@@ -86,6 +127,7 @@ const RegistrationForm = () => {
                         type="password"
                         placeholder="password"
                         minLength={8}
+                        value={password}
                         name="password"
                         onChange={(e)=>{
                             setPassword(e.target.value)
@@ -100,6 +142,7 @@ const RegistrationForm = () => {
                         type="text"
                         placeholder="firstname"
                         name="firstname"
+                        value={name.firstName||''}
                         onChange={(e)=>{
                             setName({...name, firstName:e.target.value})
                         }}
@@ -111,6 +154,7 @@ const RegistrationForm = () => {
                         type="text"
                         placeholder="lastname"
                         name="lastname"
+                        value={name.lastName||''}
                         onChange={(e)=>{
                             setName({...name, lastName:e.target.value})
                         }}
